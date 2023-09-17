8e1b011696a1e66c3a2615c653e3aeb0e93df195
linxiaoxin <lin_xiaoxin@hotmail.com>
2023-09-17 22:15:29 +0800

    Updated Joblisting Response

--- src/main/java/com/mtech/sjmsjob/entity/Job.java
+++ src/main/java/com/mtech/sjmsjob/entity/Job.java
@@ -1,7 +1,6 @@
 package com.mtech.sjmsjob.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +9,6 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
-@Builder
 public class Job {
 
     @Id
