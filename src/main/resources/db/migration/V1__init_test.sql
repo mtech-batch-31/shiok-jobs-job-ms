d5b0b738930755e25f1e4a30308bb90e3179e4c3
ernestjx <73307945+ernestjx@users.noreply.github.com>
2023-09-23 17:09:20 +0800

    updated flyway script to create table if not exist

--- src/main/resources/db/migration/V1__init_test.sql
+++ src/main/resources/db/migration/V1__init_test.sql
@@ -1,3 +1,28 @@
+CREATE TABLE IF NOT EXISTS job
+(
+    id bigint NOT NULL,
+    closing_date timestamp(6) without time zone,
+    company_id bigint NOT NULL,
+    company_name character varying(255) COLLATE pg_catalog."default",
+    created_by character varying(255) COLLATE pg_catalog."default",
+    created_time timestamp(6) without time zone,
+    employment_type character varying(255) COLLATE pg_catalog."default",
+    job_category character varying(255) COLLATE pg_catalog."default",
+    job_summary text COLLATE pg_catalog."default",
+    job_title character varying(255) COLLATE pg_catalog."default",
+    last_updated_by character varying(255) COLLATE pg_catalog."default",
+    last_updated_time timestamp(6) without time zone,
+    level character varying(255) COLLATE pg_catalog."default",
+    location character varying(255) COLLATE pg_catalog."default",
+    max_salary numeric(38,2),
+    min_salary numeric(38,2),
+    posted_date timestamp(6) without time zone,
+    skills character varying(255) COLLATE pg_catalog."default",
+    version integer,
+    work_hours character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT job_pkey PRIMARY KEY (id)
+)
+
 INSERT INTO job(
 	id, closing_date, company_id, company_name, created_by, created_time, employment_type, job_category, job_summary,
 	job_title, last_updated_by, last_updated_time, location, max_salary, min_salary, posted_date, skills,
