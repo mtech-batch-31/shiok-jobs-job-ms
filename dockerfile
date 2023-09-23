5a48bf3e48f7370812f5480f425fdb3077844970
ernestjx <73307945+ernestjx@users.noreply.github.com>
2023-09-23 16:03:12 +0800

    added flyway, but not working in docker

--- dockerfile
+++ dockerfile
@@ -2,5 +2,6 @@ FROM openjdk:17-alpine
 
 COPY build/libs/shiok_jobs_job_ms.jar /shiok_jobs_job_ms.jar
 COPY src/main/resources/application.properties /application.properties
+COPY src/main/resources/db/migration /db/migration
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "/shiok_jobs_job_ms.jar"]
---
+++ src/main/resources/db/migration/V1__init_test.sql
@@ -0,0 +1,5 @@
+INSERT INTO job(
+	id, closing_date, company_id, company_name, created_by, created_time, employment_type, job_category, job_summary,
+	job_title, last_updated_by, last_updated_time, location, max_salary, min_salary, posted_date, skills,
+	version, work_hours)
+	VALUES (1, '2023-01-01', 1, 'nus', 'shiokjobs', now(), null, null, null, null, null, null, null, null, null, null, null, null, null);
