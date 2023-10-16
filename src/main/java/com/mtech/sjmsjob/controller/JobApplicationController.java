ec6b3548b251f1ac889282ea94ac8f2af18019e8
ernestjx <73307945+ernestjx@users.noreply.github.com>
2023-10-16 22:53:36 +0800

    updated integration with accountUuid

--- src/main/java/com/mtech/sjmsjob/controller/JobApplicationController.java
+++ src/main/java/com/mtech/sjmsjob/controller/JobApplicationController.java
@@ -1,10 +1,12 @@
 package com.mtech.sjmsjob.controller;
 
 import com.mtech.sjmsjob.service.JobApplicationService;
-import com.mtech.sjmsjob.util.JwtTokenUtil;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
@@ -17,18 +19,19 @@ public class JobApplicationController {
         this.jobApplService = jobApplService;
     }
     @PostMapping("/apply")
-    public ResponseEntity applyJob(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody String jobId) throws IllegalArgumentException {
+    public ResponseEntity applyJob(@RequestHeader("user-id") String accountUuid, @RequestBody String jobId) throws IllegalArgumentException {
         try
         {
-            if(token.isEmpty())
-                return ResponseEntity.badRequest().body("Unknown user");
-            var username = new JwtTokenUtil().getUserNameFromJWT(token);
-            if(username.isEmpty())
-                return ResponseEntity.badRequest().body("Unknown user");
+//            if(token.isEmpty())
+//                return ResponseEntity.badRequest().body("Unknown user");
+//            var username = new JwtTokenUtil().getUserNameFromJWT(token);
+//            if(username.isEmpty())
+//                return ResponseEntity.badRequest().body("Unknown user");
             long jobIdval = Long.valueOf(jobId);
-            jobApplService.applyJob(UUID.fromString(username), jobIdval);
+            jobApplService.applyJob(UUID.fromString(accountUuid), jobIdval);
         } catch(Exception ex)
         {
+            ex.printStackTrace();
             return ResponseEntity.internalServerError().body(false);
         }
         return ResponseEntity.ok("Job applied successfully");
