aa81036143935e21fa05b8cd12214aa15eacc6f9
ernestjx <73307945+ernestjx@users.noreply.github.com>
2023-10-28 02:15:17 +0800

    commented out unused code

--- src/main/java/com/mtech/sjmsjob/util/JwtTokenUtil.java
+++ src/main/java/com/mtech/sjmsjob/util/JwtTokenUtil.java
@@ -5,18 +5,18 @@ import java.util.Base64;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Component;
 
-@Component
-public class JwtTokenUtil {
-
-    public String getUserNameFromJWT(String token) {
-        var tokenParts = token.split("\\.");
-        String tokenWithoutSignature = tokenParts[1];
-        Base64.Decoder decoder = Base64.getDecoder();
-        var tokenWithoutSignatureDecode = new String(decoder.decode(tokenWithoutSignature));
-
-        var jsonParser = JsonParserFactory.getJsonParser();
-        var map = jsonParser.parseMap(tokenWithoutSignatureDecode);
-        var username = (String) map.get("username");
-        return username;
-    }
-}
+//@Component
+//public class JwtTokenUtil {
+//
+//    public String getUserNameFromJWT(String token) {
+//        var tokenParts = token.split("\\.");
+//        String tokenWithoutSignature = tokenParts[1];
+//        Base64.Decoder decoder = Base64.getDecoder();
+//        var tokenWithoutSignatureDecode = new String(decoder.decode(tokenWithoutSignature));
+//
+//        var jsonParser = JsonParserFactory.getJsonParser();
+//        var map = jsonParser.parseMap(tokenWithoutSignatureDecode);
+//        var username = (String) map.get("username");
+//        return username;
+//    }
+//}
