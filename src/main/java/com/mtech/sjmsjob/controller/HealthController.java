f449681996527240afb6384a38ec3479d512db77
Terry Chia <chiawz@hotmail.com>
2024-04-04 00:06:11 +0800

    fix: fix sonar issue

--- src/main/java/com/mtech/sjmsjob/controller/HealthController.java
+++ src/main/java/com/mtech/sjmsjob/controller/HealthController.java
@@ -1,13 +1,14 @@
 package com.mtech.sjmsjob.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/jobs")
+@RequestMapping(value = "/v1/jobs")
 public class HealthController {
 
-    @RequestMapping("/actuator/health")
+    @RequestMapping(value = "/actuator/health", method = RequestMethod.GET)
     public String healthCheck() {
         return "OK";
     }
